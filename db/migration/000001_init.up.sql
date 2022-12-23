31a59f237f0cb55d7fd1085c182dd5a545f64b24
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-23 02:31:38 +0100

    🍞 gen sqlc user's code

--- db/migration/000001_init.up.sql
+++ db/migration/000001_init.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE "users" (
   "id" varchar PRIMARY KEY,
   "username" varchar UNIQUE NOT NULL,
   "name" varchar NOT NULL,
+  "password" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
--- db/query/user.sql
+++ db/query/user.sql
@@ -1,1 +1,11 @@
+-- name: CreateUser :one
+INSERT INTO users (
+	id, username, email, password, name 
+)VALUES(
+	$1, $2, $3, $4, $5
+) RETURNING *;
 
+-- name: GetUser :one 
+SELECT * FROM users 
+WHERE username = $1
+OR email = $1 LIMIT 1;
